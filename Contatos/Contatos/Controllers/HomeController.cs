5a18598e83aef0888ce1786a05aaba387b033089
Fernando Borel <fernandomenezesborel@gmail.com>
2023-01-20 17:52:16 -0300

    Alterações gerais

--- Contatos/Contatos/Controllers/HomeController.cs
+++ Contatos/Contatos/Controllers/HomeController.cs
@@ -11,16 +11,14 @@ namespace Contatos.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        
         public IActionResult Index()
         {
-            return View();
+            HomeModel home = new HomeModel();
+            home.Nome = "Fer";
+            home.Email = "fer@gmail.com";
+
+            return View(home);
         }
 
         public IActionResult Privacy()
